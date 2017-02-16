40eb0171cf791d4f5cd5fb599f52d63374a2091d
HrafnkellTorri <Kallinnhannhrabbi@hotmail.com>
2017-02-16 15:00:06 +0000

    Add files via upload

---
+++ myproject/application/view/home/index.php
@@ -0,0 +1,4 @@
+<div class="container">
+    <h1>Hello World</h1>
+    <p>Þetta er Verkefni nr.3 í VEF</p>
+</div>
---
+++ myproject/application/view/home/page1.php
@@ -0,0 +1,4 @@
+<div class="container">
+    <h1>Hello World</h1>
+    <p>Síða 1</p>
+</div>
